4e653942f6129b4f9bd8be816626d905a28e1628
HaiNV123 <93862703+HaiNV123@users.noreply.github.com>
2022-02-23 09:45:30 -0800

    update source code

--- js/autoInput.js
+++ js/autoInput.js
@@ -1,11 +1,10 @@
 var option = {
     strings: [
-        "<span class='tag'>&lt;</span><span class='tagname'>div</span> <span class='artribute'>class</span><span class='artributeValue'>='p-xl m-xl bg-black-50 b-sthin b-solid b-black-400 shadow1'</span><span class='tag'>&gt;</span><span class='tagvalue'>This is Irepub CSS</span><span class='tag'>&lt;/</span><span class='tagname'>div</span><span class='tag'>&gt;</span>",
+        "<span class='tag'>&lt;</span><span class='tagname'>div</span> <span class='artribute'>class</span><span class='artributeValue'>='p-xl m-xl bg-black-50 b-sthin b-solid b-black-400 shadow1'</span><span class='tag'>&gt;</span><span class='tagvalue'>This is Twippo CSS</span><span class='tag'>&lt;/</span><span class='tagname'>div</span><span class='tag'>&gt;</span>",
         "<span class='tag'>&lt;</span><span class='tagname'>div</span> <span class='artribute'>class</span><span class='artributeValue'>='p-m m-m c-blue-500 b-medium b-dotted b-blue-400 shadow2'</span><span class='tag'>&gt;</span><span class='tagvalue'>It helps you to create UI faster.</span><span class='tag'>&lt;/</span><span class='tagname'>div</span><span class='tag'>&gt;</span>",
         ],
-    typeSpeed: 40,
-    backSpeed: 20,
-    cursorChar: '|',
+    typeSpeed: 10,
+    backSpeed: 10,
     loop: true,
 }
 var typed = new Typed(".autoinput", option)
